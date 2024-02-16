3628f6ed7545e80d341c364b705531814aba6e3a
ext_rjparis <rjparis@falabella.cl>
2024-02-16 13:23:09 -0300

    add in operator

--- internal/repository/generic_repository.go
+++ internal/repository/generic_repository.go
@@ -24,14 +24,70 @@ func NewRepository(client *firestore.Client) *GenericRepository {
 	return repositorySingleton
 }
 
+const chunkSize = 30
+
 func (r GenericRepository) Query(collection string, filters []Filter, limit int) ([]interface{}, error) {
 	coll := r.firestore.Collection(collection)
+	hasInCondition, filterInCondition := getFilterWithInCondition(filters)
+	if hasInCondition {
+		results, err := r.queryWithChunks(filters, filterInCondition, coll, limit)
+		if err != nil {
+			return nil, err
+		}
+		return results, nil
+	}
+	return r.runQuery(filters, coll, limit)
+}
+
+func (r GenericRepository) queryWithChunks(filters []Filter, filterInCondition *Filter, coll *firestore.CollectionRef, limit int) ([]interface{}, error) {
+	valueArray := filterInCondition.Value.([]interface{})
+	chunks := getChunks(valueArray)
+
+	var allResults []interface{}
+	for _, chunk := range chunks {
+		filtersChunk := duplicateWithChangedFirstValue(filters, chunk)
+		results, err := r.runQuery(filtersChunk, coll, limit)
+		if err != nil {
+			return nil, err
+		}
+		if len(results) > 0 {
+			allResults = append(allResults, results...)
+		}
+	}
+
+	return allResults, nil
+}
+
+func getChunks(values []interface{}) [][]interface{} {
+	var chunks [][]interface{}
+	for i := 0; i < len(values); i += chunkSize {
+		end := i + chunkSize
+		if end > len(values) {
+			end = len(values)
+		}
+		chunks = append(chunks, values[i:end])
+	}
+	return chunks
+}
+
+func duplicateWithChangedFirstValue(filters []Filter, newValue interface{}) []Filter {
+	duplicate := make([]Filter, len(filters))
+	copy(duplicate, filters)
+	duplicate[0].Value = newValue
+	return duplicate
+}
+
+func (r GenericRepository) runQuery(filters []Filter, coll *firestore.CollectionRef, limit int) ([]interface{}, error) {
 	var query firestore.Query
 	for i, filter := range filters {
+		value := filter.Value
+		if filter.Operation == "in" {
+			value = filter.Value.([]interface{})
+		}
 		if i == 0 {
-			query = coll.Where(filter.Field, filter.Operation, filter.Value)
+			query = coll.Where(filter.Field, filter.Operation, value)
 		} else {
-			query = query.Where(filter.Field, filter.Operation, filter.Value)
+			query = query.Where(filter.Field, filter.Operation, value)
 		}
 	}
 
@@ -56,5 +112,13 @@ func (r GenericRepository) Query(collection string, filters []Filter, limit int)
 	}
 
 	return data, nil
+}
 
+func getFilterWithInCondition(filters []Filter) (bool, *Filter) {
+	for _, filter := range filters {
+		if filter.Operation == "in" {
+			return true, &filter
+		}
+	}
+	return false, nil
 }
